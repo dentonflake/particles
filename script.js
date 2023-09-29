4ae303361422b1d083c70f92ff371bdc4d0ff9ca
dentonflake <denton.flake@gmail.com>
2023-09-29 13:06:00 -0700

    changed starting files

---
+++ script.js
@@ -0,0 +1,38 @@
+// Canvas object
+let canvas = new Canvas(
+    {
+        // Rectangle collider
+        collider: new Rect(
+            new Vector2D(
+                0, // X position
+                0 // Y position
+            ),
+            900, // Width
+            600 // Height
+        )
+    }
+);
+
+let myCircle = new Circle(
+    new Vector2D(450, 300),
+    50
+);
+
+function loop() {
+    // Drawing the canvas
+    canvas.rect(
+        canvas.collider, // Rectangle collider
+        'rgb(0, 0, 0)' // Color rgb(RED [0-255], GREEN [0-255], BLUE [0-255])
+    );
+
+    canvas.circle(
+        myCircle,
+        'red'
+    );
+
+    myCircle.pos.x += 1;
+
+    requestAnimationFrame(loop);
+}
+
+loop();
