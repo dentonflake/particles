942c570c846ec87e4ab980561624079f6e7aee4f
dentonflake <denton.flake@gmail.com>
2023-09-29 13:27:06 -0700

    created particle file and added mouse position

--- script.js
+++ script.js
@@ -11,19 +11,25 @@ let canvas = new Canvas(
             innerHeight // Height
         )
     }
-);
+)
 
+let mouse = new Vector2D(0, 0)
 
 function loop() {
     // Drawing the canvas
     canvas.rect(
         canvas.collider, // Rectangle collider
-        'rgb(0, 0, 0)' // Color rgb(RED [0-255], GREEN [0-255], BLUE [0-255])
-    );
+        new Color(25, 25, 25) // Color rgb(RED [0-255], GREEN [0-255], BLUE [0-255])
+    )
 
-
-
-    requestAnimationFrame(loop);
+    requestAnimationFrame(loop)
 }
 
-loop();
+loop()
+
+addEventListener('mousemove', (event) => {
+    mouse.pos = {
+        x: event.clientX,
+        y: event.clientY
+    }
+})
